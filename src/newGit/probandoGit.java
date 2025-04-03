edcdd3de54081af16176a0ab2c46ed394dac2ff7
Aimar <Aimcon2@alu.edu.gva.es>
2025-04-03 16:08:17 +0200

    Segundo commit. AIMAR :)

--- src/newGit/probandoGit.java
+++ src/newGit/probandoGit.java
@@ -4,6 +4,7 @@ public class probandoGit {
 
 	public static void main(String[] args) {
 		System.out.println("Orest es el mejor y Aimar es un rayado. Primer commit, el original");
+		System.out.println("Y este es el de Aimar 😂😂");
 		
 
 	}
