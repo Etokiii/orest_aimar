6477f13aa58b148f398cac073cbb373e714e8ada
Orest <oretsokhm@alu.edu.gva.es>
2025-04-03 15:34:54 +0200

    Commit original, Orest :D:D:D:D

---
+++ src/newGit/probandoGit.java
@@ -0,0 +1,11 @@
+package newGit;
+
+public class probandoGit {
+
+	public static void main(String[] args) {
+		System.out.println("Orest es el mejor y Aimar es un rayado. Primer commit, el original");
+		
+
+	}
+
+}
