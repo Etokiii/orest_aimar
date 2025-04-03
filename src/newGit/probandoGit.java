64b31116fa15bed1c1fefe56f6dfa573f47dd40f
Orest <oretsokhm@alu.edu.gva.es>
2025-04-03 17:24:50 +0200

    Tercer y ultimo commit. Orest :P

--- src/newGit/probandoGit.java
+++ src/newGit/probandoGit.java
@@ -5,6 +5,7 @@ public class probandoGit {
 	public static void main(String[] args) {
 		System.out.println("Orest es el mejor y Aimar es un rayado. Primer commit, el original");
 		System.out.println("Y este es el de Aimar 😂😂");
+		System.out.println("Finally thye olast one, gby Orest. Un kissis");
 		
 
 	}
